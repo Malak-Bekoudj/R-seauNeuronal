f976621982963a2cbee84cdd206378edde5f7424
Malak-Bekoudj <malakbekoudj@gmail.com>
2025-04-04 02:37:32 +0100

    "Data preparation"

--- ReseauNeuronal.py
+++ ReseauNeuronal.py
@@ -4,12 +4,13 @@ print("\n=== Étape 1: Préparer les données ===")
 
 donnees =np.loadtxt("data.txt")
 points_3D=donnees[:, :3]
-etiquettes=donnees[:, 3] 
+etiquettes=donnees[:, 3]   # 1 = vase, 0 = bruit
 
 points_3D=(points_3D-points_3D.mean(axis=0))/points_3D.std(axis=0)
-
 X_train,X_test,y_train,y_test=train_test_split(points_3D,etiquettes,test_size=0.3)
 
 print(f"Nombre total de points : {len(points_3D)}")
 print(f"Points vase : {int(etiquettes.sum())}")
-    
+print("\nExemple de point normalisé :")
+print(f"Coordonnées : {X_train[0]}")
+print(f"Étiquette : {y_train[0]}")    
